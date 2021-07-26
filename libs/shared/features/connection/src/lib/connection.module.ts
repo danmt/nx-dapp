521f5101b31e9cec46022e7751bfd0b2b9631d50
Daniel Marin <danielarturomt@gmail.com>
2021-07-26 11:47:59 +0200

    fix: remove routing logic from connection module

--- libs/shared/features/connection/src/lib/connection.module.ts
+++ libs/shared/features/connection/src/lib/connection.module.ts
@@ -3,17 +3,13 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import { Route, RouterModule } from '@angular/router';
 
 import { ConnectionsDropdownComponent } from './connections-dropdown.component';
-
-export const connectionRoutes: Route[] = [];
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterModule,
     MatFormFieldModule,
     MatSelectModule,
   ],
