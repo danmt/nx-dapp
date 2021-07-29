e444c9b2de407aa42b214572567a5c6b9baa8ed8
Daniel Marin <danielarturomt@gmail.com>
2021-07-29 21:57:32 +0200

    chore(account-adapter): remove commented code

--- libs/solana/account-adapter/rx/src/lib/state.ts
+++ libs/solana/account-adapter/rx/src/lib/state.ts
@@ -5,28 +5,10 @@ export const accountInitialState: AccountState = {
   userAccounts: [],
   tokenAccounts: [],
   nativeAccount: null,
-  /* connection: null,
-  walletPublicKey: null,
-  walletConnected: false, */
 };
 
 export const reducer = (state: AccountState, action: Action) => {
   switch (action.type) {
-    /* case 'loadConnection':
-      return {
-        ...state,
-        connection: (action as LoadConnectionAction).payload,
-      };
-    case 'loadWalletPublicKey':
-      return {
-        ...state,
-        walletPublicKey: (action as LoadWalletPublicKeyAction).payload,
-      };
-    case 'loadWalletConnected':
-      return {
-        ...state,
-        walletConnected: (action as LoadWalletConnectedAction).payload,
-      }; */
     case 'loadTokenAccounts':
       return {
         ...state,
