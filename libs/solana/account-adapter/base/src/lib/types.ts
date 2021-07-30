f57f320d65d3cf098554f8a1a184e1a8c2fe4c4f
Daniel Marin <danielarturomt@gmail.com>
2021-07-30 16:44:06 +0200

    feat(utils): generate an u64 library

--- libs/solana/account-adapter/base/src/lib/types.ts
+++ libs/solana/account-adapter/base/src/lib/types.ts
@@ -1,5 +1,19 @@
-import { AccountInfo as TokenAccountInfo } from '@solana/spl-token';
 import { AccountInfo, PublicKey } from '@solana/web3.js';
+import { u64 } from '@nx-dapp/solana/utils/u64';
+
+export interface TokenAccountInfo {
+  address: PublicKey;
+  mint: PublicKey;
+  owner: PublicKey;
+  amount: u64;
+  delegate: null | PublicKey;
+  delegatedAmount: u64;
+  isInitialized: boolean;
+  isFrozen: boolean;
+  isNative: boolean;
+  rentExemptReserve: null | u64;
+  closeAuthority: null | PublicKey;
+}
 
 export interface TokenAccount {
   pubkey: PublicKey;
---
+++ libs/solana/utils/u64/README.md
@@ -0,0 +1,7 @@
+# solana-utils-u64
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test solana-utils-u64` to execute the unit tests via [Jest](https://jestjs.io).
