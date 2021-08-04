f6ff601941146a1184bcfa8b7c155f04310c6870
Daniel Marin <danielarturomt@gmail.com>
2021-08-04 23:32:03 +0200

    feat(wallet): set up a config for getBalances

--- libs/solana-dapp/wallet/utils/get-balances/src/lib/get-balances.ts
+++ libs/solana-dapp/wallet/utils/get-balances/src/lib/get-balances.ts
@@ -3,18 +3,15 @@ import { forkJoin, Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
 import { mapToBalances, observeUserAccounts } from './operators';
-import { Balance } from './types';
+import { Balance, GetBalancesConfig } from './types';
 import { getMintAccounts, getNativeAccount, getTokenAccounts } from './utils';
 
-export const getBalances = (
-  rpcEndpoint: string,
-  walletPublicKey: string
-): Observable<Balance[]> =>
-  of(new Connection(rpcEndpoint, 'recent')).pipe(
+export const getBalances = (config: GetBalancesConfig): Observable<Balance[]> =>
+  of(new Connection(config.rpcEndpoint, 'recent')).pipe(
     switchMap((connection) =>
       forkJoin([
-        getNativeAccount(connection, new PublicKey(walletPublicKey)),
-        getTokenAccounts(connection, new PublicKey(walletPublicKey)),
+        getNativeAccount(connection, new PublicKey(config.walletPublicKey)),
+        getTokenAccounts(connection, new PublicKey(config.walletPublicKey)),
       ]).pipe(
         observeUserAccounts(connection),
         switchMap((userAccounts) =>
