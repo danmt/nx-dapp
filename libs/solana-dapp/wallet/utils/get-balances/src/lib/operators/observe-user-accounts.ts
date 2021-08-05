968e6e5e32d529c59ca555ee0474ce737d460b8b
Daniel Marin <danielarturomt@gmail.com>
2021-08-05 12:26:44 +0200

    feat(account): make from account change a generic

--- libs/solana-dapp/wallet/utils/get-balances/src/lib/operators/observe-user-accounts.ts
+++ libs/solana-dapp/wallet/utils/get-balances/src/lib/operators/observe-user-accounts.ts
@@ -1,7 +1,7 @@
 import { TokenAccount } from '@nx-dapp/solana-dapp/account/types';
 import { fromAccountChangeEvent } from '@nx-dapp/solana-dapp/account/utils/from-account-change';
 import { TokenAccountParser } from '@nx-dapp/solana-dapp/account/utils/serializer';
-import { Connection } from '@solana/web3.js';
+import { AccountInfo, Connection } from '@solana/web3.js';
 import { combineLatest, Observable } from 'rxjs';
 import { concatMap, map, startWith, switchMap } from 'rxjs/operators';
 
@@ -20,7 +20,10 @@ const fromTokenAccountChangeEvent = (
   connection: Connection,
   tokenAccount: TokenAccount
 ) =>
-  fromAccountChangeEvent(connection, tokenAccount.pubkey).pipe(
+  fromAccountChangeEvent<AccountInfo<Buffer>>(
+    connection,
+    tokenAccount.pubkey
+  ).pipe(
     map((account) => TokenAccountParser(tokenAccount.pubkey, account)),
     startWith(tokenAccount)
   );
