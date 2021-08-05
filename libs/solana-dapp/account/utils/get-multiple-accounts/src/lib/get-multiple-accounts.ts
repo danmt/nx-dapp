7f4341c20e14893b9f03e493de66ed9223637cb8
Daniel Marin <danielarturomt@gmail.com>
2021-08-05 18:42:24 +0200

    refactor(account): replace by method provided in Connection

--- libs/solana-dapp/account/utils/get-multiple-accounts/src/lib/get-multiple-accounts.ts
+++ libs/solana-dapp/account/utils/get-multiple-accounts/src/lib/get-multiple-accounts.ts
@@ -1,71 +1,17 @@
-import { AccountInfo } from '@solana/web3.js';
-import { defer, from, Observable } from 'rxjs';
+import { Commitment, Connection, PublicKey } from '@solana/web3.js';
+import { defer, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-function chunks<T>(array: T[], size: number): T[][] {
-  return Array.apply<number, T[], T[][]>(
-    0,
-    new Array(Math.ceil(array.length / size))
-  ).map((_, index) => array.slice(index * size, (index + 1) * size));
-}
-
-const getMultipleAccountsCore = async (
-  connection: any,
-  keys: string[],
-  commitment: string
-) => {
-  const args = connection._buildArgs([keys], commitment, 'base64');
-
-  const unsafeRes = await connection._rpcRequest('getMultipleAccounts', args);
-  if (unsafeRes.error) {
-    throw new Error(
-      'failed to get info about account ' + unsafeRes.error.message
-    );
-  }
-
-  if (unsafeRes.result.value) {
-    const array = unsafeRes.result.value as AccountInfo<string[]>[];
-    return { keys, array };
-  }
-
-  // TODO: fix
-  throw new Error();
-};
-
 export const getMultipleAccounts = (
-  connection: any,
+  connection: Connection,
   keys: string[],
-  commitment: string
-): Observable<{ keys: string[]; array: AccountInfo<Buffer>[] }> =>
+  commitment: Commitment
+) =>
   from(
     defer(() =>
-      Promise.all(
-        chunks(keys, 99).map((chunk) =>
-          getMultipleAccountsCore(connection, chunk, commitment)
-        )
+      connection.getMultipleAccountsInfo(
+        keys.map((key) => new PublicKey(key)),
+        commitment
       )
     )
-  ).pipe(
-    map((result) => ({
-      array: result
-        .map(
-          (a) =>
-            a.array
-              .map((acc) => {
-                if (!acc) {
-                  return undefined;
-                }
-
-                const { data, ...rest } = acc;
-                const obj = {
-                  ...rest,
-                  data: Buffer.from(data[0], 'base64'),
-                } as AccountInfo<Buffer>;
-                return obj;
-              })
-              .filter((_) => _) as AccountInfo<Buffer>[]
-        )
-        .reduce((flatten, list) => [...flatten, ...list], []),
-      keys,
-    }))
-  );
+  ).pipe(map((accounts) => ({ array: accounts || [], keys: keys || [] })));
