4ba66fdd6115e713fcb46d401e66677b29cff24e
Daniel Marin <danielarturomt@gmail.com>
2021-08-09 21:45:04 +0200

    refactor(wallet): use webclient as base for extend in wallet service

--- libs/solana-dapp/angular/src/lib/services/wallet.service.ts
+++ libs/solana-dapp/angular/src/lib/services/wallet.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import { Network } from '@nx-dapp/solana-dapp/network';
 import {
   DEFAULT_WALLET,
   getPhantomWallet,
@@ -9,56 +8,38 @@ import {
   WalletClient,
   WalletName,
 } from '@nx-dapp/solana-dapp/wallet';
-import { Subject } from 'rxjs';
 import { map, takeUntil } from 'rxjs/operators';
 
 import { SolanaDappNetworkService } from '.';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class SolanaDappWalletService implements OnDestroy {
-  private readonly destroy = new Subject();
-  private readonly _walletClient = new WalletClient(
-    [
-      getSolletWallet(),
-      getPhantomWallet(),
-      getSolongWallet(),
-      getSolflareWallet(),
-    ],
-    DEFAULT_WALLET
-  );
-  wallets$ = this._walletClient.wallets$;
-  selectedWallet$ = this._walletClient.selectedWallet$;
-  connected$ = this._walletClient.connected$;
-  connecting$ = this._walletClient.connecting$;
-  disconnecting$ = this._walletClient.disconnecting$;
-  walletAddress$ = this._walletClient.publicKey$.pipe(
+@Injectable()
+export class SolanaDappWalletService extends WalletClient implements OnDestroy {
+  walletAddress$ = this.publicKey$.pipe(
     map((publicKey) => publicKey?.toBase58() || null)
   );
 
   constructor(private solanaDappNetworkService: SolanaDappNetworkService) {
+    super(
+      [
+        getSolletWallet(),
+        getPhantomWallet(),
+        getSolongWallet(),
+        getSolflareWallet(),
+      ],
+      DEFAULT_WALLET
+    );
+
     this.solanaDappNetworkService.network$
-      .pipe(takeUntil(this.destroy))
+      .pipe(takeUntil(this.destroy$))
       .subscribe((network) => this.setNetwork(network));
   }
 
   ngOnDestroy() {
-    this.destroy.next();
-    this.destroy.complete();
-    this._walletClient.destroy();
+    this.destroy();
   }
 
-  setNetwork(network: Network) {
-    this._walletClient.setNetwork(network);
-  }
-
-  connect(walletName: WalletName) {
-    this._walletClient.selectWallet(walletName);
-    setTimeout(() => this._walletClient.connect());
-  }
-
-  disconnect() {
-    this._walletClient.disconnect();
+  selectAndConnect(walletName: WalletName) {
+    this.selectWallet(walletName);
+    setTimeout(() => this.connect());
   }
 }
