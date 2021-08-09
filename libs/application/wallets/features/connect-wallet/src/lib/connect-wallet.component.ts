9d7f0b7b8b115ebd22ea77d475eec28740fb3430
Daniel Marin <danielarturomt@gmail.com>
2021-08-09 21:10:59 +0200

    feat(wallet): connect wallet with a single click (#40)

--- libs/application/wallets/features/connect-wallet/src/lib/connect-wallet.component.ts
+++ libs/application/wallets/features/connect-wallet/src/lib/connect-wallet.component.ts
@@ -1,17 +1,9 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  HostBinding,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import {
   SolanaDappWalletService,
   WalletName,
 } from '@nx-dapp/solana-dapp/angular';
-import { Subject } from 'rxjs';
-import { filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'nx-dapp-connect-wallet',
@@ -21,37 +13,23 @@ import { filter, takeUntil } from 'rxjs/operators';
       <p>Pick your option and press connect.</p>
     </header>
 
-    <div class="flex flex-col-reverse gap-8">
+    <div class="flex flex-wrap justify-around gap-4">
       <button
-        mat-stroked-button
-        color="primary"
-        class="uppercase w-full h-12"
-        [disabled]="(selectedWallet$ | async) === null"
-        (click)="onConnectWallet()"
+        *ngFor="let wallet of wallets$ | async"
+        (click)="onConnectWallet(wallet.name)"
       >
-        <ng-container *ngIf="(connecting$ | async) === false; else connecting">
-          Connect
-        </ng-container>
-      </button>
-
-      <div class="flex flex-wrap justify-around gap-4">
-        <button
-          *ngFor="let wallet of wallets$ | async"
-          (click)="onWalletSelected(wallet.name)"
+        <figure
+          class="w-24 h-24 p-4 opacity-30 hover:opacity-100"
+          [ngClass]="{
+            'opacity-100': (selectedWallet$ | async) === wallet.name
+          }"
         >
-          <figure
-            class="w-24 h-24 p-4 opacity-30 hover:opacity-100"
-            [ngClass]="{
-              'opacity-100': (selectedWallet$ | async) === wallet.name
-            }"
-          >
-            <img [src]="wallet.icon" class="w-4/5 h-4/5 mx-auto" alt="" />
-            <figcaption class="mt-1 text-center">
-              {{ wallet.name }}
-            </figcaption>
-          </figure>
-        </button>
-      </div>
+          <img [src]="wallet.icon" class="w-4/5 h-4/5 mx-auto" alt="" />
+          <figcaption class="mt-1 text-center">
+            {{ wallet.name }}
+          </figcaption>
+        </figure>
+      </button>
     </div>
 
     <button
@@ -62,17 +40,12 @@ import { filter, takeUntil } from 'rxjs/operators';
     >
       <mat-icon>close</mat-icon>
     </button>
-
-    <ng-template #connecting>
-      <mat-spinner color="primary" diameter="28" class="mx-auto"></mat-spinner>
-    </ng-template>
   `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ConnectWalletComponent implements OnInit, OnDestroy {
+export class ConnectWalletComponent {
   @HostBinding('class') class = 'block w-72 relative';
-  private readonly _destroy = new Subject();
   wallets$ = this.walletService.wallets$;
   selectedWallet$ = this.walletService.selectedWallet$;
   connecting$ = this.walletService.connecting$;
@@ -82,25 +55,8 @@ export class ConnectWalletComponent implements OnInit, OnDestroy {
     private dialogRef: MatDialogRef<ConnectWalletComponent>
   ) {}
 
-  ngOnInit() {
-    this.walletService.connected$
-      .pipe(
-        filter((connected) => connected),
-        takeUntil(this._destroy)
-      )
-      .subscribe(() => this.dialogRef.close());
-  }
-
-  ngOnDestroy() {
-    this._destroy.next();
-    this._destroy.complete();
-  }
-
-  onWalletSelected(walletName: WalletName) {
-    this.walletService.selectWallet(walletName);
-  }
-
-  onConnectWallet() {
-    this.walletService.connect();
+  onConnectWallet(walletName: WalletName) {
+    this.walletService.connect(walletName);
+    this.dialogRef.close();
   }
 }
