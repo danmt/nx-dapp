51dc680cb385ef076e98582cc0473f5005da238b
Daniel Marin <danielarturomt@gmail.com>
2021-08-10 16:29:40 +0200

    feat(wallet): set up basic form for sending funds

--- libs/application/wallets/features/send-funds/src/lib/send-funds.component.ts
+++ libs/application/wallets/features/send-funds/src/lib/send-funds.component.ts
@@ -1,20 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'nx-dapp-send-funds',
   template: `
-    <p>
-      send-funds works!
-    </p>
+    <header nxDappModalHeader class="mr-8">
+      <h1>Send funds</h1>
+      <p>Complete the form and hit send.</p>
+    </header>
+
+    <form [formGroup]="sendFundsGroup" class="flex flex-col gap-4">
+      <mat-form-field class="w-full" appearance="fill">
+        <mat-label>Recipient</mat-label>
+        <input matInput formControlName="recipient" required />
+        <mat-hint *ngIf="!submitted || sendFundsGroup.get('recipient')?.valid"
+          >Enter the receiver's address.</mat-hint
+        >
+        <mat-error
+          *ngIf="submitted && sendFundsGroup.get('recipient')?.errors?.required"
+          >The recipient is mandatory.</mat-error
+        >
+      </mat-form-field>
+
+      <mat-form-field class="w-full">
+        <mat-label>Amount</mat-label>
+        <input matInput formControlName="amount" type="number" required />
+        <mat-hint>Maximum amount is ?</mat-hint>
+        <mat-error
+          *ngIf="submitted && sendFundsGroup.get('amount')?.errors?.required"
+          >The amount is mandatory.</mat-error
+        >
+      </mat-form-field>
+
+      <p class="text-xs text-center text-warn m-0">
+        Make sure you're sending the funds to the right address.
+      </p>
+
+      <button
+        mat-stroked-button
+        color="primary"
+        (click)="onSendFunds()"
+        class="w-full"
+        [disabled]="submitted && sendFundsGroup.invalid"
+      >
+        Send funds
+      </button>
+    </form>
+
+    <button
+      mat-icon-button
+      aria-label="Close connection attempt"
+      class="w-8 h-8 leading-none absolute top-0 right-0"
+      mat-dialog-close
+    >
+      <mat-icon>close</mat-icon>
+    </button>
   `,
-  styles: [
-  ]
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SendFundsComponent implements OnInit {
+export class SendFundsComponent {
+  @HostBinding('class') class = 'block w-72 relative';
+  submitted = false;
+  sendFundsGroup = new FormGroup({
+    recipient: new FormControl('', [Validators.required]),
+    amount: new FormControl(null, [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(private dialogRef: MatDialogRef<SendFundsComponent>) {}
 
-  ngOnInit(): void {
+  onSendFunds() {
+    this.submitted = true;
+
+    if (this.sendFundsGroup.valid) {
+      console.log(this.sendFundsGroup.value);
+      this.dialogRef.close();
+    }
   }
-
 }
--- libs/application/wallets/features/send-funds/src/lib/send-funds.module.ts
+++ libs/application/wallets/features/send-funds/src/lib/send-funds.module.ts
@@ -1,11 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { ModalHeaderModule } from '@nx-dapp/shared/ui/modal-header';
 
 import { SendFundsComponent } from './send-funds.component';
 
 @NgModule({
-  imports: [CommonModule, MatDialogModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    ModalHeaderModule,
+  ],
   declarations: [SendFundsComponent],
   exports: [SendFundsComponent],
 })
