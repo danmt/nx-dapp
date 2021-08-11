836cc98affb50ea1cd0107f161bb99696597b7ca
Daniel Marin <danielarturomt@gmail.com>
2021-08-11 22:52:48 +0200

    chore: take out unnused

--- libs/solana-dapp/transaction/src/lib/types.ts
+++ libs/solana-dapp/transaction/src/lib/types.ts
@@ -1,13 +1,6 @@
-import { Connection, Transaction as Web3Transaction } from '@solana/web3.js';
+import { Transaction as Web3Transaction } from '@solana/web3.js';
 
 export interface Transaction {
   id: string;
   data: Web3Transaction;
-}
-
-export interface TransferConfig {
-  connection: string | Connection;
-  walletAddress: string;
-  recipientAddress: string;
-  amount: number;
 }
